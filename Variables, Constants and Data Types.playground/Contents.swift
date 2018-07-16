faed9b4bf3fddd3d6df557e09e3087d681814896
aofleejay <aofleejay@hotmail.com>
2018-07-16 21:43:17 +0700

    Variables, Constants and Data Types

--- Variables, Constants and Data Types.playground/Contents.swift
+++ Variables, Constants and Data Types.playground/Contents.swift
@@ -1,5 +1,19 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+// Variables
+var age: Int = 25
+age = 26
+
+// Constants
+let firstname: String = "Kunapot"
+let lastname: String = "Pairat"
+
+// Concatnation
+let fullName: String = "\(firstname) \(lastname)"
+
+// Data types
+let name: String = "Kunapot"
+let price: Int = 99
+let tax: Float = 1.99
+let latitude: Double = 10.1405823041294
+let isOpen: Bool = true
