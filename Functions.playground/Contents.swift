496018d0a34fc6c8171542f7e94c0c0f49c3b449
aofleejay <aofleejay@hotmail.com>
2018-07-16 23:33:34 +0700

    Learn function by create calculate BMI function

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,7 +1,15 @@
 import UIKit
 
-func power(base: Double, exponent: Double) -> Double {
-    return pow(base, exponent)
+func calculateBMI(weight: Double, height: Double) -> Double {
+    return weight / pow(height / 100, 2)
 }
 
-power(base: 2.5, exponent: 5)
+let BMI = calculateBMI(weight: 60.5, height: 167.5)
+
+if BMI > 25 {
+    print("Your BMI is \(BMI), You are overweight.")
+} else if BMI >= 18.5 {
+    print("Your BMI is \(BMI), You are normal weight.")
+} else {
+    print("Your BMI is \(BMI), You are underweight.")
+}
